1f66f594cdd8c2fa4b6dde5886be239bff980e29
Aditya Yadav <166515021+aditya0yadav@users.noreply.github.com>
2024-10-29 19:01:55 +0530

    improve and add more functionality in a server.js

--- server.js
+++ server.js
@@ -1,3 +1,7 @@
 const captureIpAddress = require('./core/iptracker');
+const customIPBlocker = require('./core/ipblocker');
 
-module.exports = captureIpAddress;
+module.exports = {
+  captureIpAddress, 
+  customIPBlocker
+};
