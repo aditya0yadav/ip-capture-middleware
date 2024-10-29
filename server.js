80dcdcde880ef0afefc0babc700d2080db69ae48
Aditya Yadav <166515021+aditya0yadav@users.noreply.github.com>
2024-10-29 19:39:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,9 @@
 const captureIpAddress = require('./core/iptracker');
+const customIPBlocker = require('./core/ipblocker');
+const getGeolocation = require('./core/ipgeo');
 
-module.exports = captureIpAddress;
+module.exports = {
+  captureIpAddress, 
+  customIPBlocker,
+  getGeolocation
+};
